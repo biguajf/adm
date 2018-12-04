36ccbb0210a9262c8366fa96b15b71d11df3d4d5
julioGurgel <jfgurgel@yahoo.com.br>
2018-12-04 18:17:36 -0200

    primeiro commit

--- gentelella/settings.py
+++ gentelella/settings.py
@@ -151,9 +151,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'django_project', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
